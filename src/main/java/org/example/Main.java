ba691344c1a17a58c4a10c33e7270ab962f2e973
andrey <telezhenko.aa@mail.ru>
2024-03-20 21:57:19 +0300

    add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printThreeWords();
+        checkSumSign();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple" );
+            }
+
+    public static void checkSumSign() {
+        int a = 5; // Пример значения для переменной a
+        int b = -3; // Пример значения для переменной b
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 }
