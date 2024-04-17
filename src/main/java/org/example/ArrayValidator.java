7581bcbdd992cbeb961b4bbae26bb0feda7963fa
andrey_t <pacanraw@icloud.com>
2024-04-17 06:17:42 +0300

    add fix homework lession 5

--- src/main/java/org/example/ArrayValidator.java
+++ src/main/java/org/example/ArrayValidator.java
@@ -2,16 +2,18 @@ package org.example;
 
 public class ArrayValidator {
     public int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
-        if (array.length != 4 || array[0].length != 4) {
+        if (array.length != 4) {
             throw new MyArraySizeException();
         }
-
-        int sum = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i].length != 4) {
                 throw new MyArraySizeException();
             }
-            for (int j = 0; j < array[i].length; j++) {
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+             for (int j = 0; j < array[i].length; j++) {
                 try {
                     sum += Integer.parseInt(array[i][j]);
                 } catch (NumberFormatException e) {
